84c8e034286adce234398274c72642f0a2f04a93
Aniket Chakraborty <ani@Anikets-Laptop.local>
2024-11-24 17:04:54 -0800

    Set up backend template

---
+++ backend/src/test/java/com/project/habitual/HabitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.habitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
